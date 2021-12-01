827b465f5579dd7ee9c606fce67c979930bbebd5
Valentina Gomez Valencia <71842995+valgv@users.noreply.github.com>
2021-11-30 22:27:55 -0500

    Add files via upload

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -1,9 +1,38 @@
 package vehiculos;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Fabricante extends Vehiculo {
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	private static ArrayList<Fabricante> fabricantes= new ArrayList<Fabricante>();
+	private static int cantidadVentas = 0;
 
-	public Fabricante() {
-		// TODO Auto-generated constructor stub
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre= nombre;
+		this.pais= pais;
+		fabricantes.add(this);
 	}
 
+	public String getNombre(){
+		return nombre;
+	}
+	public void setNombre(String nombre){
+		this.nombre= nombre;
+	}
+
+	public Pais getPais(){
+		return pais;
+	}
+	public void setPais(Pais pais){
+		this.pais= pais;
+	}
+
+	public static ArrayList<Fabricante> getFabricantes(){
+		return fabricantes;
+	}
+	
+	public static Fabricante fabricaMayorVentas(){
+		return Collections.max(fabricantes, cantidadVentas);
+	}
 }
